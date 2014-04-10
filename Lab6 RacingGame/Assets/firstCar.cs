a375209930e8760b842fbc1ce967b5e18b7b53d9
n8sk8gr8 <portville99@gmail.com>
2014-04-09 22:52:11 -0400

    Added cars

---
+++ Lab6 RacingGame/Assets/firstCar.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class firstCar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate (0, 0, -10f * Time.deltaTime);
+			
+		}
+
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Translate (0, 0, 10f * Time.deltaTime);
+
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate (10f * Time.deltaTime, 0, 0);
+			
+		}
+
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate (-10f * Time.deltaTime, 0, 0);
+			
+		}
+	}
+}
