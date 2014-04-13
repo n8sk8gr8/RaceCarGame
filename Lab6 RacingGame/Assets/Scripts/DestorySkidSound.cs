acadbd6befab2fb28234790fb361caf6bbad9b3d
n8sk8gr8 <portville99@gmail.com>
2014-04-13 17:36:53 -0400

    Added Simple Skidding Sound Effects

---
+++ Lab6 RacingGame/Assets/Scripts/DestorySkidSound.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestorySkidSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy (gameObject, 2f);
+	}
+}
---
+++ Lab6 RacingGame/Assets/Scripts/SkidScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkidScript : MonoBehaviour {
+	public float currentFriction;
+	float skidAt;
+	float skidSoundEmittion;
+	float skidDensity;
+	public GameObject skidSound;
+
+	// Use this for initialization
+	void Start () {
+		skidAt = 1.5f;
+		skidSoundEmittion = 10f;
+		skidDensity = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		WheelHit hit;
+		WheelCollider wheel = (WheelCollider)(collider);
+		wheel.GetGroundHit (out hit);
+		currentFriction = Mathf.Abs(hit.sidewaysSlip);
+
+		if(skidAt <= currentFriction && skidDensity <= 0)
+		{
+			Instantiate(skidSound, hit.point, Quaternion.identity);
+			skidDensity = 1;
+		}
+		skidDensity -= skidSoundEmittion * Time.deltaTime;
+	}
+}
