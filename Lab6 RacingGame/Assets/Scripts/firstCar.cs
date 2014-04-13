9b5d1479e1e1f3811f7c65ac8495f33d48d41b6d
n8sk8gr8 <portville99@gmail.com>
2014-04-13 02:29:18 -0400

    Fixed Car Flipping issue
    Adjusted the COG

--- Lab6 RacingGame/Assets/Scripts/firstCar.cs
+++ Lab6 RacingGame/Assets/Scripts/firstCar.cs
@@ -8,23 +8,42 @@ public class firstCar : MonoBehaviour {
 	public WheelCollider wheelRL;
 	public float maxTorque;
 	public float brake;
+	float topSpeed;
+	float lowSpeedTurning;
+	float highSpeedTurning;
 	// Use this for initialization
 	void Start () {
 		maxTorque = 50;
-		rigidbody.centerOfMass = new Vector3(0, -0.9f, 0);
+		rigidbody.centerOfMass = new Vector3(0, 0, 0);
 		brake = 80;
+		topSpeed = 60;
+		lowSpeedTurning = 12;
+		highSpeedTurning = 1;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		wheelRR.motorTorque = maxTorque * Input.GetAxis ("Vertical");
-		wheelRL.motorTorque = maxTorque * Input.GetAxis ("Vertical");
-		wheelFR.steerAngle = 10 * Input.GetAxis ("Horizontal");
-		wheelFL.steerAngle = 10 * Input.GetAxis ("Horizontal");
+		wheelRR.motorTorque = -maxTorque * Input.GetAxis ("Vertical");
+		wheelRL.motorTorque = -maxTorque * Input.GetAxis ("Vertical");
 
-		wheelRR.brakeTorque = 0;
-		wheelRL.brakeTorque = 0;
+		float speed = rigidbody.velocity.magnitude / topSpeed;
+		float currentSteeringAngle = Mathf.Lerp (lowSpeedTurning, highSpeedTurning, speed);
+		currentSteeringAngle *= Input.GetAxis ("Horizontal");
+		wheelFR.steerAngle = currentSteeringAngle;
+		wheelFL.steerAngle = currentSteeringAngle;
 
+		if(Input.GetKey(KeyCode.UpArrow) == false && Input.GetKey (KeyCode.DownArrow) == false)
+		{
+			wheelRL.brakeTorque = 25;
+			wheelRR.brakeTorque = 25;
+		}
+		else
+		{
+			wheelRR.brakeTorque = 0;
+			wheelRL.brakeTorque = 0;
+		}
+
+		// Brake When the spacebar is pressed
 		if (Input.GetKey (KeyCode.Space)) 
 		{
 			wheelRR.brakeTorque = brake;
