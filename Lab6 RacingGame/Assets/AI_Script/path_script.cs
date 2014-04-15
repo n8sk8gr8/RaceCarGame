be875b651ffa4155076bf32d8979b4dd5e631647
Harry Lew <harrylew25@gmail.com>
2014-04-15 03:26:56 -0400

    Added waypoints

---
+++ Lab6 RacingGame/Assets/AI_Script/path_script.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class path_script : MonoBehaviour {
+	
+	public Transform[] path;
+	public Transform[] path_objs;
+	public Color rayColor = Color.white;
+	public Vector3 pos;
+	public Vector3 prev;
+	
+	void OnDrawGizmos()
+	{
+		Gizmos.color = rayColor;
+		//pathTransform = 
+		path_objs = this.GetComponentsInChildren <Transform>();
+		path = new Transform[path_objs.Length];
+
+		for (int i = 0; i < path_objs.Length; i++) 
+		{
+			path[i] = path_objs[i];
+		}
+		
+		for (int i=0; i<path.Length; i++) 
+		{
+			pos = path[i].position;
+			if(i>0)
+			{
+				pos = path[i].position;
+				if(i>0)
+				{
+					prev = path[i-1].position;
+				}
+				Gizmos.DrawLine(prev,pos);
+				Gizmos.DrawWireSphere(pos,0.3f);
+			}
+		}
+	}
+}
