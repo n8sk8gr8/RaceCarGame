4f4c015d377f642a961a39cc852e559fe9565abb
Harry Lew <harrylew25@gmail.com>
2014-04-28 19:59:14 -0400

    Road Repaired and added trees
    hello

--- Lab6 RacingGame/Assets/AI_Script/AIWheelScript.cs
+++ Lab6 RacingGame/Assets/AI_Script/AIWheelScript.cs
@@ -4,19 +4,19 @@ using System.Collections;
 public class AIWheelScript : MonoBehaviour {
 
 	public WheelCollider myWheelCollider;
-
+	private Vector3 wheel;
 	// Use this for initialization
 	void Start () {
-	
+		wheel = new Vector3 (0,0,0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate(myWheelCollider.rpm*360*Time.deltaTime,0,0);
-//		transform.localEulerAngles.y = myWheelCollider.steerAngle - transform.localEulerAngles.z; 
-
-		RaycastHit hit;
-		Vector3 wheelPos;
+		wheel.y  = myWheelCollider.steerAngle - transform.localEulerAngles.z; 
+		transform.localEulerAngles = new Vector3(0 ,wheel.y, 0);
+		//RaycastHit hit;
+		//Vector3 wheelPos;
 //		if (Physics.Raycast(myWheelCollider.transform.position,-myWheelCollider.transform.up,hit,myWheelCollider.radius + myWheelCollider.suspensionDistance))  
 //			wheelPos = hit.point + myWheelCollider.transform.up * myWheelCollider.radius;  
 //		else   
